37e2016d08c9dcc5353791956a286f04e61a73fc
Tarek <tarek.elghoul.ext@transway.fr>
2022-07-27 18:49:47 +0100

    adding iOS framework to workspace

---
+++ WeatherService/WeatherService/WeatherService.h
@@ -0,0 +1,18 @@
+//
+//  WeatherService.h
+//  WeatherService
+//
+//  Created by Tarek El Ghoul on 27/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherService.
+FOUNDATION_EXPORT double WeatherServiceVersionNumber;
+
+//! Project version string for WeatherService.
+FOUNDATION_EXPORT const unsigned char WeatherServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherService/PublicHeader.h>
+
+
