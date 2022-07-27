e4f7fc6a17cc941bd44ae9ae8526a771b010e8c2
Tarek <tarek.elghoul.ext@transway.fr>
2022-07-27 18:49:09 +0100

    adding iOS app project to workspace

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Tarek El Ghoul on 27/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
