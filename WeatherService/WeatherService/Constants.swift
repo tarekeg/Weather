ec79a2d6e98f38eb70ce1bf730e8153a8150835d
Tarek <tarek.elghoul.ext@transway.fr>
2022-07-27 19:19:30 +0100

    Adding Constant file

---
+++ WeatherService/WeatherService/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  WeatherService
+//
+//  Created by Tarek El Ghoul on 27/07/2022.
+//
+
+import Foundation
+
+internal class Constants {
+
+    internal class Endpoints {
+        
+        static let API_URL = "http://api.openweathermap.org/data/2.5"
+
+        private init() {}
+    }
+
+    private init() {}
+}
