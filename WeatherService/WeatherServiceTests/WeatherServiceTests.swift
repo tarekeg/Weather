d60dd844473cf5f2011b40525ba940ebaef1be20
Tarek <tarek.elghoul.ext@transway.fr>
2022-07-29 02:02:12 +0100

    Adding test to the framework's currentWeather function

--- WeatherService/WeatherServiceTests/WeatherServiceTests.swift
+++ WeatherService/WeatherServiceTests/WeatherServiceTests.swift
@@ -9,9 +9,31 @@ import XCTest
 @testable import WeatherService
 
 class WeatherServiceTests: XCTestCase {
+    
+    override func setUp() {
+        WeatherService.initialize(withAppId: "c242ebb29379467369e6903ca9d5d832")
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    func test_currentWeather_forGeoCoordinate() {
+        var result: WeatherData?
+        var caughtError: Error?
+        let ex = expectation(description: "We should have weather data of location that we have passed")
+
+        WeatherService.instance.currentWeather(coord: (latitude: 10.58212, longitude: 38.0579958)) { weatherData, error in
+    
+            result = weatherData
+            caughtError = error
+            ex.fulfill()
+        }
+
+        wait(for: [ex], timeout: 5.0)
+
+        XCTAssertNotNil(result)
+        XCTAssertNil(caughtError)
     }
 
     override func tearDownWithError() throws {
